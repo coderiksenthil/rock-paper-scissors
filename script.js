1b557979d5679c94b2869a3e7064efcd57f34aca
U.Senthil Kumar <U.senthilkumar1@outlook.com>
2023-09-23 19:19:53 +0530

    The Fisher Yates Method

--- script.js
+++ script.js
@@ -44,3 +44,16 @@ function choice(playerselection,computerselection){
         return text;
     }
 }
+
+const points = [40, 100, 1, 5, 25, 10];
+document.getElementById("demo2").innerHTML = points;  
+
+function myFunction() {
+  for (let i = points.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i+1));
+    let k = points[i];
+    points[i] = points[j];
+    points[j] = k;
+  }
+  document.getElementById("demo2").innerHTML = points;
+}
