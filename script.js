8ace192ec41a74e0f8563104c143f286cd34702f
U.Senthil Kumar <U.senthilkumar1@outlook.com>
2023-09-28 18:40:14 +0530

    Code Update

--- script.js
+++ script.js
@@ -1,59 +1,93 @@
-function myfunction() {
+let humanScore = 0;
+let computerScore = 0;
+let playerScoreDisplay = document.querySelector(".human-score");
+let computerScoreDisplay = document.querySelector(".computer-score");
+
+let rockButton = document.querySelector("#rock");
+let paperButton = document.querySelector("#paper");
+let scissorsButton = document.querySelector("#scissors");
+
+rockButton.addEventListener('click', function() {
+    playRound('rock');
+});
+
+paperButton.addEventListener('click', function() {
+    playRound('paper');
+});
+
+scissorsButton.addEventListener('click', function() {
+    playRound('scissors');
+});
+
+/*function myfunction() {
     //let name = prompt("Enter the number of times: ", "6");
     //console.log("Hello" + name);
     //for(let i = 0; name > i; i ++)
     for(let i = 0;i<5;i++){
         start();
     } 
-}
+}*/
 
-function start(){
-    let playerselection = prompt("Rock Paper or Scissors: ")
-    playerselection = playerselection.toLowerCase();
-    let computerselection = getComputerchoice();
-    let text = choice(playerselection,computerselection);
-    console.log(text);
-    //computerreply();
-}
-
-function getComputerchoice(){
-     elements = ['rock', 'paper', 'Scissors'];
-     let random = Math.floor(Math.random()*3);
-     return elements[random];
-}
-
-function choice(playerselection,computerselection){
-    if(playerselection === "rock" && computerselection ==="Scissors"){
-        let text = "You won, rock beats paper";
-        return text;
-    }else if(playerselection === "Scissors" && computerselection === "rock"){
-        let text = "You lost, Rock beats paper";
-        return text;
-
-    }else if(playerselection === "paper" && computerselection === "rock"){
-        let text = "You Won, paper beats paper";
-        return text;
-    }else if(playerselection === "rock" && computerselection === "paper"){
-        let text = "You lost, Rock beats paper";
-        return text;
-    }else if(playerselection === computerselection){
-        let text = "its a draw";
-        return text;
-    }else{
-        let text = "you win";
-        return text;
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = determineWinner(playerSelection, computerSelection);
+    
+    updateScore(result);
+    
+    let message = document.querySelector('#msg');
+    message.textContent = result;
+    
+    if (humanScore === 5) {
+        alert("You win the game!");
+        resetGame();
+    } else if (computerScore === 5) {
+        alert("Computer wins the game!");
+        resetGame();
     }
 }
 
-const points = [40, 100, 1, 5, 25, 10];
-document.getElementById("demo2").innerHTML = points;  
-
-function myFunction() {
-  for (let i = points.length -1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i+1));
-    let k = points[i];
-    points[i] = points[j];
-    points[j] = k;
-  }
-  document.getElementById("demo2").innerHTML = points;
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let randomIndex = Math.floor(Math.random()*3);
+    return choices[randomIndex];
 }
+
+function determineWinner(player, computer) {
+    if (player === computer) {
+        return "It's a tie!";
+    } else if ((player === 'rock' && computer === 'scissors') ||
+               (player === 'paper' && computer === 'rock') ||
+               (player === 'scissors' && computer === 'paper')) {
+        return `You win! ${player} beats ${computer}.`;
+    } else {
+        return `Computer wins! ${computer} beats ${player}.`;
+    }
+}
+
+function updateScore(result) {
+    if (result.includes("You win")) {
+        humanScore++;
+    } else if (result.includes("Computer wins")) {
+        computerScore++;
+    }
+    
+    playerScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = "0";
+    computerScoreDisplay.textContent = "0";
+}
+
+/*function myFunction() {
+    for (let i = points.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i+1));
+      let k = points[i];
+      points[i] = points[j];
+      points[j] = k;
+    }
+    document.getElementById("demo2").innerHTML = points;
+  }*/
